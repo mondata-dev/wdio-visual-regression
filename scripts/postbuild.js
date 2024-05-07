c2bc4458f5548d616f95387967302313bdcc4253
David Poetzsch-Heffter <git@poehe.de>
2024-05-07 14:04:17 +0200

    fixed build

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,15 +1,24 @@
 const { resolve } = require('path');
-const { copyFileSync, existsSync } = require('fs');
+const { copyFileSync, existsSync, readFileSync, writeFileSync } = require('fs');
 
 
 const TYPINGS_FILE = 'index.d.ts';
 
 function moveTypingsToBuild() {
   const typings = resolve(process.cwd(), `./typings/${ TYPINGS_FILE }`);
-  const build = resolve(process.cwd(), './build');
+  const buildDir = resolve(process.cwd(), './build');
+  const buildTypes = resolve(buildDir, `./${ TYPINGS_FILE }`);
 
-  if (existsSync(typings) && existsSync(build)) {
-    copyFileSync(typings, resolve(build, TYPINGS_FILE));
+  if (existsSync(typings) && existsSync(buildDir)) {
+    if (existsSync(buildTypes)) {
+      // merge typings
+      const typingsContent = readFileSync(typings, 'utf8');
+      const buildTypesContent = readFileSync(buildTypes, 'utf8');
+      const mergedTypings = typingsContent + '\n\n' + buildTypesContent;
+      writeFileSync(buildTypes, mergedTypings, 'utf8');
+    } else {
+      copyFileSync(typings, buildTypes);
+    }
   }
 }
 
